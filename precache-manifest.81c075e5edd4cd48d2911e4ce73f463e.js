88bc32a294f6a310957d205b69b9bc05fc15b1cf
mania125 <alsgurmaniadev@gmail.com>
2020-04-10 18:03:18 +0900

    Updates

---
+++ precache-manifest.81c075e5edd4cd48d2911e4ce73f463e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7104f595c0641a23578ed935552cd9ad",
+    "url": "/begin-react/index.html"
+  },
+  {
+    "revision": "373e3079e520041b887e",
+    "url": "/begin-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d51a89e0fc3d3aa5850",
+    "url": "/begin-react/static/js/2.d9cfcbd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/begin-react/static/js/2.d9cfcbd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373e3079e520041b887e",
+    "url": "/begin-react/static/js/main.3c10af34.chunk.js"
+  },
+  {
+    "revision": "63035625da08685d9200",
+    "url": "/begin-react/static/js/runtime-main.ed6596b0.js"
+  }
+]);
